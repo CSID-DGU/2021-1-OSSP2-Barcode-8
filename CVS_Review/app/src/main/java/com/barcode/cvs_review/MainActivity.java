9639de3ec5b9d614a9880a9e0468466d3cfd7855
RieL <choyj1222@gmail.com>
2021-05-02 02:26:16 +0900

    Fixed Button to ImageButton Casting
    - Reviewed Commit (4bd2633)

--- CVS_Review/app/src/main/java/com/barcode/cvs_review/MainActivity.java
+++ CVS_Review/app/src/main/java/com/barcode/cvs_review/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-
 public class MainActivity extends AppCompatActivity{
     MainFragment mainFragment;
     SevenFragment sevenFragment;
--- CVS_Review/app/src/main/java/com/barcode/cvs_review/MainFragment.java
+++ CVS_Review/app/src/main/java/com/barcode/cvs_review/MainFragment.java
@@ -6,15 +6,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainFragment extends Fragment {
-
+    ViewGroup rootView;
+    ImageButton button1;
+    ImageButton button2;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main, container, false);
+        rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main, container, true);
 
-        Button button1 = rootView.findViewById(R.id.Button1);
+        button1 = (ImageButton)rootView.findViewById(R.id.Button1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,7 +26,7 @@ public class MainFragment extends Fragment {
             }
         });
 
-        Button button2 = rootView.findViewById(R.id.Button2);
+        button2 = (ImageButton)rootView.findViewById(R.id.Button2);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
