ed95facd5b27c87badab0fc4b5d5d1252f5a9d60
Daehyun Yun <71585151+eogus0512@users.noreply.github.com>
2021-05-02 04:33:13 +0900

    rollback

--- CVS_Review/app/src/main/java/com/barcode/cvs_review/MainActivity.java
+++ CVS_Review/app/src/main/java/com/barcode/cvs_review/MainActivity.java
@@ -5,28 +5,11 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity{
-    MainFragment mainFragment;
-    SevenFragment sevenFragment;
-    CuFragment cuFragment;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.mainFragment);
-        sevenFragment = new SevenFragment();
-        cuFragment = new CuFragment();
     }
-
-    public void onFragmentChanged(int index) {
-        if (index == 0) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, mainFragment).commit();
-        } else if (index == 1) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, sevenFragment).commit();
-        } else if (index == 2) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, cuFragment).commit();
-        }
-    }
-
 }
