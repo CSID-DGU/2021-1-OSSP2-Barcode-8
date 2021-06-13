14bf286e7b9e06be0243a2e5a8ea24b0b3a41e6c
eogus0512 <71585151+eogus0512@users.noreply.github.com>
2021-06-14 04:01:21 +0900

    리뷰 작성 엑티비티 추가
    엑티비티 생성 및 레이아웃 구성

--- CVS_Review/app/src/main/java/com/barcode/cvs_review/activity/ProductSpecActivity.java
+++ CVS_Review/app/src/main/java/com/barcode/cvs_review/activity/ProductSpecActivity.java
@@ -1,14 +1,19 @@
 package com.barcode.cvs_review.activity;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,6 +25,9 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CenterCrop;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -32,11 +40,13 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ProductSpecActivity extends AppCompatActivity {
     TextView product_name;
     ImageView product_image;
     RatingBar rating;
+    ImageView add;
     String PRODUCT_NAME;
     String PRODUCT_IMAGE;
     String AVE_GRADE;
@@ -56,6 +66,7 @@ public class ProductSpecActivity extends AppCompatActivity {
         product_image = findViewById(R.id.imageView);
         product_name = findViewById(R.id.textView);
         rating = findViewById(R.id.ratingBar);
+        add = findViewById(R.id.add);
 
         Intent intent = getIntent();
         PRODUCT_NAME = intent.getStringExtra("PRODUCT_NAME");
@@ -76,6 +87,15 @@ public class ProductSpecActivity extends AppCompatActivity {
         RequestOptions requestOptions = new RequestOptions();
         requestOptions = requestOptions.transform(new CenterCrop(), new RoundedCorners(30));
         Glide.with(getApplicationContext()).load(PRODUCT_IMAGE).apply(requestOptions).into(product_image);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProductSpecActivity.this, InsertReview.class);
+                intent.putExtra("NAME", PRODUCT_NAME);
+                startActivity(intent);
+            }
+        });
 
     }
 
@@ -216,6 +236,30 @@ public class ProductSpecActivity extends AppCompatActivity {
                 return null;
             }
 
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_actions, menu) ;
+
+        return true ;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_home:
+
+                Intent home_intent = new Intent(getApplicationContext(),MainActivity.class);
+                home_intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivityForResult(home_intent, 101);
+                return true;
+            case R.id.action_settings:
+                Intent setting_intent = new Intent(getApplicationContext(), SettingActivity.class);
+                setting_intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivityForResult(setting_intent, 101);
+                return true;
+            default :
+                return super.onOptionsItemSelected(item) ;
         }
     }
 
